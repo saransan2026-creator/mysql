7509d530b0db818293b2e7e35b8820bc69a1b062
saransan2026-creator <saransan2026@gmail.com>
2025-12-30 19:14:16 +0530

    change
    changed and now calling from env

--- config/db.ts
+++ config/db.ts
@@ -1,10 +1,13 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "mydb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
 });
 
 db.connect((err) => {
